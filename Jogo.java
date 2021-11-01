06ec8930651e445228a164d1dfd3d8bddae3e94b
Pedro Rocha <93152576+Pedro-Rocha12@users.noreply.github.com>
2021-11-01 14:14:42 +0000

    Adicionei o inicio do jogo na classe Jogo

--- Jogo.java
+++ Jogo.java
@@ -1,3 +1,69 @@
-public class Jogo{
-  
+import java.util.Scanner;
+
+public class Jogo {
+	
+	public static void choosePlace(Tabuleiro tab) {
+		Scanner l = new Scanner(System.in);
+		System.out.println("Choose a line: ");
+		int line = l.nextInt();
+		
+		Scanner c = new Scanner(System.in);
+		System.out.println("Choose a column: ");
+		int column = c.nextInt();
+		
+		tab.openCell(line, column);
+		tab.printCampoMinado();
+		
+	}
+		
+	public static void startGame(){
+	
+		Scanner d = new Scanner(System.in);
+		System.out.println("Choose the difficulty level:     |  1   |   2    |  3   |  ");
+		System.out.print("                                 | Easy | Medium | Hard |  ");
+		int dificult = d.nextInt();
+		
+		if(dificult == 1) {
+				System.out.println("Game mode set easy!");
+				Tabuleiro tab = new Tabuleiro(8, 8);  
+				tab.printCampoMinado();
+				choosePlace(tab);
+		}else if(dificult == 2){	 
+				System.out.println("Game mode set medium!");
+			  	Tabuleiro tab = new Tabuleiro(16, 16);
+			  	tab.printCampoMinado();
+			  	choosePlace(tab);
+		}else if(dificult == 3) {		
+				System.out.println("Game mode set hard!");
+				Tabuleiro tab = new Tabuleiro(24, 24);
+				tab.printCampoMinado();
+				choosePlace(tab);
+		}else{
+				System.out.println();
+				System.out.println("This level does not exist! Try again.");
+				}			
+	}
+	
+	public static void main(String[] args) {	
+		int escolha = 0;
+		while(escolha < 5){
+			startGame();
+			escolha++;
+		}
+		 	
+	}
 }
+
+
+// Tabuleiro tab = new Tabuleiro(8, 8);
+// tab.printCampoMinado();
+
+// System.out.println();
+
+// tab.setFlag(5, 5, true);
+// tab.printCampoMinado();
+
+// System.out.println();
+
+// tab.openCell(4, 3);
+// tab.printCampoMinado();
