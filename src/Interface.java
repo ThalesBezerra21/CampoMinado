4eb404942e9c7ceba84c5d82c80a934da3b67bb2
Pedro-Rocha12 <93152576+Pedro-Rocha12@users.noreply.github.com>
2021-11-23 20:36:30 -0300

    Adicionando um inicio de interface

---
+++ src/Interface.java
@@ -0,0 +1,18 @@
+
+public interface Interface {
+	
+	public static final int larguraFacil = 8;
+	public static final int larguraMedio = 12;
+	public static final int larguraDificil = 16;
+	
+	public abstract void setDificuldade();
+	
+	public abstract void setMaluco();
+	
+	public abstract void choosePlace();
+	
+	public abstract void setFlag();
+	
+	public abstract void openCell();
+	
+}
